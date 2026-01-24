4e7c51b3b2ac615bf15435a76cf27f5ca8b8a9e0
deepanshusinghrajpoot <deepanshusingh20022004@gmail.com>
2026-01-24 08:32:02 +0530

    update

--- RouteHookConcept/src/sql/0_Theoretical/0_BasicOfDatabase/1_WhatIsRDBMS.jsx
+++ RouteHookConcept/src/sql/0_Theoretical/0_BasicOfDatabase/1_WhatIsRDBMS.jsx
@@ -11,14 +11,10 @@ lll RDBMS :- Is come into 1970
 DBMS                                                           RDBMS
  
 DBMS application store **data as file**                        RDBMS applications store **data in a tabular form**
-
 **Normalization is not** present in DBMS.                      **Normalization is** present in RDBMS
-
 DBMS does **not apply any security** with regards              RDBMS **defines the integrity constraint** for the purpose of ACID
 to data manipulation.                                          (Atomacity, Consistency, Isolation and Durability) property.
-
-DBMS **does not support distributed database**.                RDBMS **supports distributed database**.
-                       
+DBMS **does not support distributed database**.                RDBMS **supports distributed database**.             
 Example of DBMS are file systems, xml etc.                     Example of RDBMS are **mysql, postgre, sql server, oracle** etc.
 
 
